624efb0af105400c153cc37c51889e92e296abc0
Justin Tew <justin-@live.com>
2019-04-29 20:22:11 -0700

    finished lesson 7

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Sherbert Socks',
-        inStock: true,
-        image: './assets/img/socks1.jpg',
+        brand: 'Gabriel',
+        product: 'Socks',
+        selectedVariant: 0,
         link: 'https://www.gabrielbrand.com/collections/accesories',
         details: ['One size fits most', '100% Cotton'],
         variants: [
             {
                 id: 1,
-                color: 'White',
-                image: './assets/img/socks1.jpg'
+                color: '#ffffff',
+                image: './assets/img/socks1.jpg',
+                quantity: 10
             },
             {
                 id: 2,
-                color: 'Blue',
-                image: './assets/img/socks2.jpg'
+                color: '#0000ff',
+                image: './assets/img/socks2.jpg',
+                quantity: 0
+            },
+            {
+                id: 3,
+                color: '#fb7f00',
+                image: './assets/img/socks3.jpg',
+                quantity: 20
             }
         ],
         cart: 0
     }, 
     methods: {
         addToCart: function() {
-            this.cart += 1;
+            this.cart += 1
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        updateProduct: function(i) {
+            this.selectedVariant = i
         }
 
+    },
+    computed: {
+        title: function() {
+            return this.brand + ' ' + this.product;
+        },
+        image: function() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock: function() {
+            return this.variants[this.selectedVariant].quantity;
+        }
     }
 })
